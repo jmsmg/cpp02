39a7b3b93c35ad627868964a6d675222c40c145d
jmsmg <jmsmg1@me.com>
2024-02-16 10:16:47 +0900

    선언문 기본

---
+++ ex00/Fixed.cpp
@@ -0,0 +1,31 @@
+#include "Fixed.hpp"
+
+Fixed::Fixed()
+{
+
+}
+
+Fixed::~Fixed()
+{
+
+}
+
+Fixed::Fixed(const Fixed &Fixed)
+{
+
+}
+
+int	Fixed::getRawBits(void) const
+{
+
+}
+
+void	Fixed::setRawbits(int const raw)
+{
+
+}
+
+void	Fixed::toInt(void)
+{
+
+}
