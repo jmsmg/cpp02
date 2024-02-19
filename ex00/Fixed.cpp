5cf307a767afc0b3707d1bf8d65e9bcc86c4ea3a
jmsmg <jmsmg1@me.com>
2024-02-19 17:18:49 +0900

    ok

--- ex00/Fixed.cpp
+++ ex00/Fixed.cpp
@@ -2,27 +2,38 @@
 
 Fixed::Fixed()
 {
-
+	std::cout << "Default constructor called" << std::endl;
 }
 
 Fixed::~Fixed()
 {
-
+	std::cout << "Destructor called" << std::endl;
 }
 
 Fixed::Fixed(const Fixed &Fixed)
 {
+	this->_width;
+	this->_binary;
+	std::cout << "Copy constructor called" << std::endl;
+}
 
+Fixed&	Fixed::operator=(const Fixed &fixed)
+{
+	if (this != &fixed)
+	{
+		std::cout << "Copy assignment operator called" << std::endl;
+	}
+	return (*this);
 }
 
 int	Fixed::getRawBits(void) const
 {
-
+	std::cout << "getRawBits member function called" << std::endl;
+	return (this->_binary);
 }
 
 void	Fixed::setRawbits(int const raw)
 {
-
 }
 
 void	Fixed::toInt(void)
